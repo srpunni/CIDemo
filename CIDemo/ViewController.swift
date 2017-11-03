82fc50053d00882f65a2998d0f85f3207b96b7e2
srpunni <srpunni@gmail.com>
2017-11-03 13:05:02 +0530

    asas

--- CIDemo/ViewController.swift
+++ CIDemo/ViewController.swift
@@ -12,7 +12,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-       // lll
+        var ss : Int = 0
+       // llladsaddasd
         // Do any additional setup after loading the view, typically from a nib.
     }
 
