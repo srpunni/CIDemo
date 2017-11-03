b6513d7a2e3ee0889e4b8c2f8e50685ceb59c369
srpunni <srpunni@gmail.com>
2017-11-03 12:56:39 +0530

    ss

--- CIDemo/ViewController.swift
+++ CIDemo/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        lll
+       // lll
         // Do any additional setup after loading the view, typically from a nib.
     }
 
